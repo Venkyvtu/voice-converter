7ecfc6cd98b631ee57d06f2324c6cfdfe849e311
Venkyvtu <145888823+Venkyvtu@users.noreply.github.com>
2023-12-12 12:13:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const Tesseract = require('tesseract.js');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 3005;
+
+app.use(express.static('public'));
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+
+const fileUpload = require("express-fileupload");
+const pdfParse = require("pdf-parse");
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/upload', upload.single('image'), (req, res) => {
+  const imageBuffer = req.file.buffer;
+
+  Tesseract.recognize(
+    imageBuffer,
+    'eng',
+    { logger: info => console.log(info) }
+  ).then(({ data: { text } }) => {
+    res.send(text);
+  }).catch(error => {
+    res.status(500).send('Error: ' + (error.message || error));
+  });
+});
+app.use(fileUpload());
+app.post("/extract-text", (req, res) => { 
+    if (!req.files && !req.files.pdfFile) 
+    { res.status(400); 
+      res.end();
+}
+pdfParse(req.files.pdfFile).then(result => {
+     res.send(result.text);
+
+});
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
